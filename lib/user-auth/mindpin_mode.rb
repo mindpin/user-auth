d5723134d1d1768d72500ec05417bf721e8bca94
fushang318 <fushang318@gmail.com>
2014-05-27 09:49:24 +0800

    add get_by_secret

--- lib/user-auth/mindpin_mode.rb
+++ lib/user-auth/mindpin_mode.rb
@@ -3,6 +3,7 @@ require 'rest_client'
 module UserAuth
   module MindpinMode
     AUTH_URL = "http://4ye.mindpin.com/account/sign_in"
+    USER_INFO_URL  = "http://4ye.mindpin.com/api/user_info"
 
     def self.included(base)
       base.extend ClassMethods
@@ -21,18 +22,42 @@ module UserAuth
         params = {:user => {:login => email, :password => password}}
         response = RestClient.post(AUTH_URL, params)
         return if response.code != 200
-        info = JSON.parse(response)
-        store = find_or_create_by(:email => info["email"])
+
+        user_info = JSON.parse(response)
+        _create_or_update_user_info(user_info)
+      rescue
+        nil
+      end
+
+      def get_by_secret(secret)
+        store = where(:secret => secret).first
+        return store if !store.blank?
+
+        response = RestClient.get(_user_info_url(secret))
+        return if response.code != 200
+
+        user_info = JSON.parse(response)
+        return if user_info["email"].blank?
+
+        _create_or_update_user_info(user_info)
+      rescue
+        nil
+      end
+
+      def _create_or_update_user_info(user_info)
+        store = find_or_create_by(:email => user_info["email"])
         store.update_attributes(
-          :name   => info["name"], 
-          :avatar => info["avatar"], 
-          :uid    => info["id"], 
-          :secret => info["secret"]
+          :name   => user_info["name"], 
+          :avatar => user_info["avatar"], 
+          :uid    => user_info["id"], 
+          :secret => user_info["secret"]
         )
         store.save
         store
-      rescue
-        nil
+      end
+
+      def _user_info_url(value)
+        "#{USER_INFO_URL}?secret=#{value}"
       end
     end
   end
--- spec/mindpin_mode_spec.rb
+++ spec/mindpin_mode_spec.rb
@@ -15,4 +15,16 @@ describe UserAuth::MindpinMode do
       user.should == nil
     }
   end
+
+  describe "get_by_secret" do
+    it{
+      secret = "ae7ea4f083de9a0ff02e1ca6f927b3ad"
+      user = MindpinModeUser.get_by_secret(secret)
+      user.name.should == "mindpin_test"
+      user.email.should == "test@mindpin.com"
+
+      user = MindpinModeUser.get_by_secret("taasdfsdf")
+      user.should == nil
+    }
+  end
 end
